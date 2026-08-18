ecc4b95460bba1184afa49947fd3c9df84591d70
Enrique Carranco <enriquec@vivaldi.net>
2026-08-17 20:49:09 -0600

    fix: use --out flag (not --output) for Gomplate v5

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,5 +2,5 @@
 set -e
 export BIND_ADDR="0.0.0.0:${PORT:-8081}"
 export POSTGRES_DSN="${DATABASE_URL}"
-gomplate --file samus.tmpl --output samus.toml
+gomplate --file samus.tmpl --out samus.toml
 exec ./main --config ./samus.toml

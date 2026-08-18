09e4daff8b5e8b3b5dc02e9e7627023147493c04
Enrique Carranco <enriquec@vivaldi.net>
2026-08-17 18:11:54 -0600

    deploy: map Vercel DATABASE_URL to POSTGRES_DSN in entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 set -e
 export BIND_ADDR="0.0.0.0:${PORT:-8081}"
+export POSTGRES_DSN="${DATABASE_URL}"
 gomplate --input samus.tmpl --output samus.toml
 exec ./main --config ./samus.toml

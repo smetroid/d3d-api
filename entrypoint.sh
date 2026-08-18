c5450e9a70f485c89dffd1d16641844a756715e5
Enrique Carranco <enriquec@vivaldi.net>
2026-08-17 20:48:45 -0600

    fix: use --file flag for Gomplate v5

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,5 +2,5 @@
 set -e
 export BIND_ADDR="0.0.0.0:${PORT:-8081}"
 export POSTGRES_DSN="${DATABASE_URL}"
-gomplate --input samus.tmpl --output samus.toml
+gomplate --file samus.tmpl --output samus.toml
 exec ./main --config ./samus.toml
